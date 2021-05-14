1bb05d992427f692437f41fbf557748713f2ea6d
Steve Dever <stephendever.cr@gmail.com>
2021-05-14 16:29:17 -0400

    update apiRoute

--- Develop/apiRoutes.js
+++ Develop/apiRoutes.js
@@ -2,14 +2,17 @@ const fs = require('fs')
 
 module.exports = (app) => {
 
-let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
- app.get('/api/notes', (req, res) => res.json(noteList));
+ app.get('/api/notes', (req, res) => {
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  return res.json(noteList)});
 
 
  app.post('/api/notes', (req, res) => {
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
  noteList.push({...req.body });
-   });
+ fs.writeFileSync('./db/db.json',JSON.stringify(noteList))
+  return res.json(noteList) });
 
 //app.delete('/api/notes/:id', (req, res) => {  
 //let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
