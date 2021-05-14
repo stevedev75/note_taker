beb9c8eb7e5502d4aacdcd9a0aa205e09b1b3387
Steve Dever <stephendever.cr@gmail.com>
2021-05-14 10:51:30 -0400

    add updated js files to test

--- Develop/public/assets/js/apiRoutes.js
+++ Develop/public/assets/js/apiRoutes.js
@@ -1,13 +1,30 @@
-// API ROUTES//
+const fs = require('fs')
 
-const notesData = require('../../../db/db.json');
+
+
 
 module.exports = (app) => {
 
-  app.get('/api/notes', (req, res) => res.json(notesData));
+  let noteList = JSON.parse(fs.readFileSync('../../../db/db.json', 'utf8'));
+
+  app.get('/api/notes', (req, res) => res.json(noteList));
 
   app.post('/api/notes', (req, res) => {
-    notesData.push(req.body);
-    // how to add to db.json file too ?? //
+    let lastID;
+    if (noteList.length) {
+      lastID = Math.max(...noteList(noteList.map(note => note.id)));
+    } else {
+        lastID = 0;
+    }
+    const id = lastId + 1;
+    noteList.push({ id, ...req.body });
+
+    res.json(noteList.slice(-1));
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+      let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
+      noteList.splice(noteList.indexOf(findNote), 1);
+      res.end("Note was deleted");  
   });
 };
