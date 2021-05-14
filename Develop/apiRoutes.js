40bebe23f850a9be739ae6e040f7ffa7eed7ef0e
Steve Dever <stephendever.cr@gmail.com>
2021-05-14 14:50:51 -0400

    update server.js file

--- Develop/apiRoutes.js
+++ Develop/apiRoutes.js
@@ -1,31 +1,19 @@
 const fs = require('fs')
 
-
-
-
 module.exports = (app) => {
 
-  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
  app.get('/api/notes', (req, res) => res.json(noteList));
 
 
-   app.post('/api/notes', (req, res) => {
-    let lastID;
-    if (noteList.length) {
-      lastID = Math.max(...noteList(noteList.map(note => note.id)));
-    } else {
-        lastID = 0;
-    }
-    const id = lastId + 1;
-    noteList.push({ id, ...req.body });
+ app.post('/api/notes', (req, res) => {
+ noteList.push({...req.body });
+   });
 
-    res.json(noteList.slice(-1));
-    });
-
-    app.delete('/api/notes/:id', (req, res) => {  
-      let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
-      noteList.splice(noteList.indexOf(findNote), 1);
-      res.end("Note was deleted");  
+app.delete('/api/notes/:id', (req, res) => {  
+let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
+noteList.splice(noteList.indexOf(findNote), 1);
+res.end("Note was deleted");  
   });
-};
+ };
--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
-const path = require('path');
-const fs = require('fs');
+//const path = require('path');
+//const fs = require('fs');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static('public'));
-
 
 
 app.use(express.urlencoded({ extended: true }));
