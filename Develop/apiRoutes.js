513db39c1c2c5aa253c542d49ecaf4cca046e10e
Steve Dever <stephendever.cr@gmail.com>
2021-05-15 14:25:09 -0400

    add id functionality to api routes

--- Develop/apiRoutes.js
+++ Develop/apiRoutes.js
@@ -1,22 +1,21 @@
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
+
 
 module.exports = (app) => {
 
 
  app.get('/api/notes', (req, res) => {
-  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-  return res.json(noteList)});
+  let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  return res.json(data)});
 
 
  app.post('/api/notes', (req, res) => {
-  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
- noteList.push({...req.body });
- fs.writeFileSync('./db/db.json',JSON.stringify(noteList))
-  return res.json(noteList) });
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  data.push(newNote);
+  fs.writeFileSync('./db/db.json',JSON.stringify(data));
+  return res.json(data) });
 
-//app.delete('/api/notes/:id', (req, res) => {  
-//let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
-//noteList.splice(noteList.indexOf(findNote), 1);
-//res.end("Note was deleted");  
-//  });
  };
